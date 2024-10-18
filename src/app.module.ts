3f5eccfa34b6a42535d859bf6b6ba6def78888dc
Juan Ochando <juanochando00@gmail.com>
2024-10-18 18:53:17 +0200

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import config from './config/database/typeorm.config';
 import { NetworkModule } from './modules/network/network.module';
 import { WalletModule } from './modules/wallet/wallet.module';
 import { AppConfigModule } from './config/app.config';
@@ -11,12 +10,13 @@ import { BlockchainModule } from './modules/blockchain/blockchain.module';
 import { TokenDataModule } from './modules/tokenData/tokenData.module';
 import { TokenModule } from './modules/token/token.module';
 import { UtilsModule } from './shared/utils/utils.module';
+import { DatabaseConfig } from './config/database/database.config';
 
 @Module({
   imports: [
     AppConfigModule,
     CacheModule.register({ isGlobal: true }),
-    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forRoot(DatabaseConfig.getDataSourceOptions()),
     UtilsModule,
     NetworkModule,
     WalletModule,
