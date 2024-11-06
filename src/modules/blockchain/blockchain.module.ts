bac83337af82cbf412d2d1802f19c06220da75f2
Juan Ochando <juanochando00@gmail.com>
2024-11-06 23:20:13 +0100

    fix

--- src/modules/blockchain/blockchain.module.ts
+++ src/modules/blockchain/blockchain.module.ts
@@ -6,9 +6,10 @@ import { WalletModule } from '../wallet/wallet.module';
 import { NetworkModule } from '../network/network.module';
 import { ProtocolModule } from './protocols/protocol.module';
 import { TokenModule } from '../token/token.module';
+import { ExternalModule } from 'src/external/external.module';
 
 @Module({
-  imports: [WalletModule, NetworkModule, ProtocolModule, TokenModule],
+  imports: [WalletModule, NetworkModule, ProtocolModule, TokenModule, ExternalModule],
   exports: [BlockchainService],
   controllers: [BlockchainController],
   providers: [BlockchainService],
