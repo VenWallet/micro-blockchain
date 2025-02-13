c618fac4562017514baafb889bf8bde651b613ce
Juan Ochando <juanochando00@gmail.com>
2025-02-13 01:15:15 +0100

    up

--- src/modules/spotMarket/spotMarket.service.ts
+++ src/modules/spotMarket/spotMarket.service.ts
@@ -227,7 +227,7 @@ export class SpotMarketService {
 
         if (side === 'SELL' ? quantity < minNotional : quantity * price < minNotional) {
           throw new BadRequestException(
-            `Cantidad demasiado baja. Debe ser al menos ${side === 'SELL' ? (minNotional * 1.05 * price).toFixed(2) : minNotional} ${side === 'BUY' ? symbol.quoteAsset : symbol.baseAsset}`,
+            `Cantidad demasiado baja. Debe ser al menos ${side === 'SELL' ? Number((minNotional * 1.05 * price).toFixed(2)) + 1 : minNotional + 1} ${side === 'BUY' ? symbol.quoteAsset : symbol.baseAsset}`,
           );
         }
 
@@ -636,7 +636,7 @@ export class SpotMarketService {
 
         if (side === 'SELL' ? quantity < minNotional : quantity * price < minNotional) {
           throw new BadRequestException(
-            `Cantidad demasiado baja. Debe ser al menos ${side === 'SELL' ? (minNotional * 1.05 * price).toFixed(2) : minNotional} ${side === 'BUY' ? symbol.quoteAsset : symbol.baseAsset}`,
+            `Cantidad demasiado baja. Debe ser al menos ${side === 'SELL' ? Number((minNotional * 1.05 * price).toFixed(2)) + 1 : minNotional + 1} ${side === 'BUY' ? symbol.quoteAsset : symbol.baseAsset}`,
           );
         }
 
